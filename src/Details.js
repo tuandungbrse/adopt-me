9617f4ac4b33e1b81ea09fbae2630afb698c33ac
Truong Nguyen Tuan Dung <tuandungbrse@gmail.com>
2020-11-13 15:17:24 +0700

    add babel config for parcel to using class properties

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import pet from '@frontendmasters/pet';
+export default class Details extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     loading: true
+  //   };
+  // }
 
-export default function Details(props) {
-  return (
-    <pre>
-      <code>${JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
+  state = { loading: true };
+
+  componentDidMount() {
+    pet
+      .animal(+this.props.id)
+      .then(({ animal }) => {
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false
+        });
+      })
+      .catch((err) => this.setState({ error: err }));
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>loading … </h1>;
+    }
+
+    const { animal, breed, location, description, media, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
