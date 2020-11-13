a461f8449844f97833fd0bf5bb4a9621d88c5509
Truong Nguyen Tuan Dung <tuandungtn@Truongs-MacBook-Pro.local>
2020-11-13 12:50:57 +0700

    jsx, useState and customer hooks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  return React.createElement('div', { id: 'app' }, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      id: 'pet1',
-      name: 'Dog 1',
-      animal: 'Dog',
-      breed: 'Hello Dog 1',
-    }),
-    React.createElement(Pet, {
-      name: 'Cat 1',
-      animal: 'Cat',
-      breed: 'Hello Cat 1',
-    }),
-    React.createElement(Pet, {
-      name: 'Bird 1',
-      animal: 'Bird',
-      breed: 'Hello Bird 1',
-    }),
-  ]);
+  return (
+    <div id="app">
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 render(React.createElement(App), document.getElementById('root'));
