19e1f233c824000f96929b86680bb1460fee5e43
Truong Nguyen Tuan Dung <tuandungbrse@gmail.com>
2020-11-13 14:16:17 +0700

    async call to fetch data & update Pet compoent

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-export default function Pet({ name, animal, breed }) {
+export default function Pet(props) {
+  let hero = 'http://placecorgi.com/300/300';
+  if (props.media.length) {
+    hero = props.media[0].small;
+  }
+
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${props.id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={props.name} />
+      </div>
+      <div className="info">
+        <h1>{props.name}</h1>
+        <h2>{`${props.animal} - ${props.breed} - ${props.location}`}</h2>
+      </div>
+    </a>
   );
 }
